c919f93ddd5c6e3dfa50360ddd2cc8caf8e46195
gabrieldaloiso <gabriel.daloiso@isen.yncrea.fr>
2025-10-02 12:18:23 +0200

    just change the name of dart page

--- pro_chatbot/lib/welcome_page.dart
+++ pro_chatbot/lib/welcome_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'sign_up.dart';
-import 'login.dart';
+import 'sign_up_page.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(const MyApp());
