017046afd52470f598a4a0083d79c4027deeea74
ArvidGrob <arvidgrobbe@gmail.com>
2025-11-12 10:54:18 +0100

    fixed bug when middlename null

--- pro_chatbot/lib/models/user.dart
+++ pro_chatbot/lib/models/user.dart
@@ -3,26 +3,29 @@ class User {
   final String email;
   final String firstname;
   final String lastname;
-  final String middlename;
+  final String? middlename;
+  final String role;
 
   User({
     required this.id,
     required this.email,
     required this.firstname,
     required this.lastname,
-    required this.middlename,
+    this.middlename,
+    required this.role,
   });
 
   // Convert JSON to User object
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'],
-      email: json['email'],
       firstname: json['firstname'],
       lastname: json['lastname'],
       middlename: json['middlename'],
+      email: json['email'],
+      role: json['role'],
     );
   }
 
-  String get fullName => '$firstname $middlename $lastname';
+  // String get fullName => '$firstname $middlename $lastname';
 }
