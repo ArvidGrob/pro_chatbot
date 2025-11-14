e2add670fc786e226cdc7a1d85a428a46b94f660
Leroy <116569478+LeroyRodermond@users.noreply.github.com>
2025-11-14 10:08:58 +0100

    Changes and merge

--- pro_chatbot/lib/admindashboard/admin_dashboard.dart
+++ pro_chatbot/lib/admindashboard/admin_dashboard.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '/theme_manager.dart';
 import '/wave_background_layout.dart';
-import 'package:pro_chatbot/admindashboard/addstudent.dart';
 import 'package:pro_chatbot/admindashboard/teacher_overview.dart';
 import 'student_overview.dart';
 import 'class_overview.dart';
@@ -46,7 +45,7 @@ class _AdminDashboardState extends State<AdminDashboard> {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              // Title + Welcome Message
+              // Title + Welcome Message ofr user
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
